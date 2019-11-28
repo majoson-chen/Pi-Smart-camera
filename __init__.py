a0500b7a663226b07d4ca3a6001feb5ff91e64a3
M-Jay <1376031164@qq.com>
2019-11-28 23:02:58 +0800

    Alpha Ver. 1.0

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ import picamera # 摄像头操作支持库
 # 参考手册:https://blog.csdn.net/m0_37509650/article/details/80282646
 # [官方文档] https://picamera.readthedocs.io/en/release-1.13/
 # https://blog.csdn.net/talkxin/article/details/50504601
-import os , time , ftplib
+import os , time , ftplib , sys , pywifi , json
 import threading as TR
 
 if os.name == 'posix':
@@ -37,7 +37,7 @@ for i in os.listdir (''):
         break
 else:
     try:
-        os.mkdir ('video',mode=0o777) # 如果没有被 break 说明文件夹没有被创建
+        os.mkdir ('video',mode=0o755) # 如果没有被 break 说明文件夹没有被创建
     except:
         print ('ERROR : video dir was created failed.')
         quit ()
@@ -48,10 +48,9 @@ else:
 EXIT = False # 退出标志符
 TH_LIST = []
 class CODEING_STATE ():
-    get = None
-    FREE = 0 # 空闲
-    RECODING = 1 # 录制中
-    BUSY = 2 # 转码 & 上传中
+    RECODING = False
+    UPLOADING = False
+    DECODEING = False
 # === FTP===
 FTP_HOST = '192.168.3.11'
 FTP_USER = 'ftp_127_0_0_1'
@@ -60,6 +59,8 @@ FTP_PORT = 21
 FTP_BUFSIZE = 1500 # 缓冲区大小
 FTP_REFRESH_CLOCK = 600 # FTP服务器每次刷新的间隔
 #FTP_ENCODING = 'utf-8' 
+REMOVE_CYCLE = 7 # 自动删除大于该天数的视频
+
 # ===================== 常量赋值 ============================
 
 GPIO.setmode (GPIO.BCM)
@@ -75,22 +76,47 @@ PICM.framerate = 24 # 帧率
 PICM.resolution = (1920,1080) # 1080p
 # ====================== PiCamera初始化 ===========================
 
-# def getFileName (path): 
-#     '''
-#         传入一个文本,返回在最后一个 '/' 之后的全部字符
-#         例如: '/a/b/c.exe' 返回 'c.exe'
-#     '''
-#     index = 0
-#     for i in path[::-1]:
-#         if i == '/':
-#             break
-#         index += 1
- 
-#     return path [-1:0-index+1]
 def getTimeStamp (format='%Y-%m-%d-%a-%H-%M-%S'): # 获取时间戳
     '''获取时间戳'''
     return time.strftime(format,time.localtime())     #把传入的元组按照格式，输出字符串
 # ======================= 辅助函数 ===============================
+
+def load_config ():
+    global FTP_BUFSIZE,FTP_HOST,FTP_PASSWD,FTP_PORT,FTP_REFRESH_CLOCK,FTP_USER,REMOVE_CYCLE
+    try:
+        with open ('/boot/camera_config.json','r') as config_file:
+            cfg = json.load (config_file)
+            FTP_BUFSIZE = cfg['FTP']['FTP_BUFSIZE']
+            FTP_HOST = cfg['FTP']['FTP_HOST']
+            FTP_PASSWD = cfg['FTP']['FTP_PASSWD']
+            FTP_PORT = cfg['FTP']['FTP_PORT']
+            FTP_REFRESH_CLOCK = cfg['FTP']['FTP_REFRESH_CLOCK']
+            FTP_USER = cfg['FTP']['FTP_USER']
+            REMOVE_CYCLE = cfg['FTP']['REMOVE_CYCLE']
+
+            # 现在开始连接网络
+            wifi = pywifi.PyWiFi ()
+            iface = wifi.interfaces () [1] # 第二个无线网卡才是树莓派的WIFI网卡
+            if iface.status () == 4: # 4 是已经连接
+                print ('MSG: WIFI connect already. Pass reconnect.')
+            else:
+                iface.scan ()
+                time.sleep (10) # 等待扫描完毕
+                for i in iface.scan_results ():
+                    if i.ssid == cfg['NETWORK']['SSID']:
+                        i.key = cfg['NETWORK']['PASSWD']
+                        iface.connect (iface.add_network_profile (i))
+                        # 如果与比对的SSID相符
+
+
+            
+
+            
+
+    except:
+        print ("WARNING: The config file in 'boot' read failed, pass now.")
+load_config ()
+# ============== 配置读取 & 网络连接 ============================
 
 def stateListener ():
     global EXIT,CODEING_STATE,STATELED
@@ -98,14 +124,14 @@ def stateListener ():
     FREQUENCY = 0.2
     try:
         while not EXIT:
-            if CODEING_STATE.get == CODEING_STATE.BUSY: # 如果正在上传或转码
+            if CODEING_STATE.DECODEING or CODEING_STATE.UPLOADING: # 如果正在上传或转码
                 STATELED.ChangeDutyCycle (100) # 常亮
-
-            elif CODEING_STATE.get == CODEING_STATE.FREE: # 空闲, 慢速闪烁
+            elif CODEING_STATE.RECODING == False and CODEING_STATE.UPLOADING == False and CODEING_STATE.DECODEING == False: 
+                # 空闲, 慢速闪烁
                 if not (FREQUENCY == 0.2) : 
                     STATELED.ChangeFrequency (0.2)
                     FREQUENCY = 0.2
-            elif CODEING_STATE.get == CODEING_STATE.RECODING: # 录制中,快速闪烁
+            elif CODEING_STATE.RECODING: # 录制中,快速闪烁
                 if not (FREQUENCY == 12): # 如果频率不等于12,更改为12
                     STATELED.ChangeFrequency (12)
                     FREQUENCY = 12
@@ -125,7 +151,6 @@ def buttonListener ():
     while not EXIT:
         try:
             if GPIO.wait_for_edge (18,GPIO.RISING,bouncetime=200,timeout=1000) != None: # 等待按钮按下
-                CODEING_STATE.get = CODEING_STATE.RECODING # 按钮按下,开始录制 设置标记
 
                 for i in os.listdir ('video'):
                     if i == getTimeStamp('%Y-%m-%d'):
@@ -138,9 +163,15 @@ def buttonListener ():
                 
                 with open (os.path.join('video',getTimeStamp('%Y-%m-%d'),getTimeStamp())+'.h264','wb') as stream:
                     time.sleep (1)
-                    PICM.start_recording(stream,format='h264',quality=35) # 开始录制
+                    PICM.start_recording(stream,format='h264',quality=30) # 开始录制
+                    # 30 - 5Mbps 码率
+                    CODEING_STATE.RECODING = True
+                    time.sleep (3)
+                    # 至少录制三秒
+
                     GPIO.wait_for_edge (18,GPIO.FALLING,bouncetime=200) # 等待按钮再次按下
                     PICM.stop_recording () # 停止录制
+                    CODEING_STATE.RECODING = False
             
         except KeyboardInterrupt:
             EXIT = True
@@ -152,6 +183,7 @@ TH_LIST.append (TH_BUTTON_LISTENER)
 # ======================= 按钮监听开始 ==========================
 
 FTP = ftplib.FTP ()
+FTP.set_pasv (False) # 禁用被动模式,使用20端口传输
 def FTPWORKER ():
     global FTP
     # =================
@@ -218,83 +250,67 @@ def FTPWORKER ():
             if not is_online: # 判断是否掉线
                 login ()
 
-                # 遍历本地文件夹
-                ALL_LOCAL_FILE = [] # 待上传文件的绝对路径
-                for i_dirlist in os.listdir ('video'):
-                    if os.path.isdir (os.path.join('video',i_dirlist)): # 这里是日期文件夹
-                        # 如果是文件夹,则列出文件夹内所有的mp4文件
-                        for i_file in os.listdir (os.path.join('video',i_dirlist)):
-                            if i_file [-4:] == '.mp4': # 如果是mp4文件, 将绝对路径添加到 ALL_FILE
-                                ALL_LOCAL_FILE.append (os.path.join('video',i_dirlist,i_file))
-                # 本地文件遍历完成
-                
-                # 开始比对服务器信息
-                # 遍历服务器目录
-                for name,info in FTP.mlsd (): # 日期目录
-                    # mlsd 返回信息如下:
-                    # ('.' ,{'type': 'pdir', 'sizd': '4096' ... })
-                    # ('..' ,{'type': 'cdir', 'sizd': '4096' ... })
-                    # ('_DSC3265.jpg' ,{'type': 'file', 'size': '11954758' ... })
-                    # 首先列出所有 mp4 文件
-                    if info ['type'] == 'dir': # 对 Picamera 目录下的文件进行筛选
+            # 遍历本地文件夹
+            ALL_LOCAL_FILE = [] # 待上传文件的绝对路径
+            for i_dirlist in os.listdir ('video'):
+                if os.path.isdir (os.path.join('video',i_dirlist)): # 这里是日期文件夹
+                    # 如果是文件夹,则列出文件夹内所有的mp4文件
+                    for i_file in os.listdir (os.path.join('video',i_dirlist)):
+                        if i_file [-4:] == '.mp4': # 如果是mp4文件, 将绝对路径添加到 ALL_FILE
+                            ALL_LOCAL_FILE.append (os.path.join('video',i_dirlist,i_file))
+            # 本地文件遍历完成
+            
+            # 开始比对服务器信息
+            # 遍历服务器目录
+            for name,info in FTP.mlsd (): # 日期目录
+                # mlsd 返回信息如下:
+                # ('.' ,{'type': 'pdir', 'sizd': '4096' ... })
+                # ('..' ,{'type': 'cdir', 'sizd': '4096' ... })
+                # ('_DSC3265.jpg' ,{'type': 'file', 'size': '11954758' ... })
+                # 首先列出所有 mp4 文件
+                if info ['type'] == 'dir': # 对 Picamera 目录下的文件进行筛选
 
-                        for name_ , info_ in FTP.mlsd (name): # 日期目录下的文件
-                            # 列出日期目录下的所有文件
-                            if info_ ['type'] == 'file':
+                    for name_ , info_ in FTP.mlsd (name): # 日期目录下的文件
+                        # 列出日期目录下的所有文件
+                        if info_ ['type'] == 'file':
 
-                                for file_name in ALL_LOCAL_FILE:
-                                    # 如果是文件 , 在本地所有文件中进行查找 , 
-                                    # 如果找到且文件大小不相等 , 删掉服务器的版本后重新上传
-                                    if os.path.basename (file_name) == name_: # 如果文件相同
-                                        if os.path.getsize (file_name) == int(info_ ['size']):
-                                            # 如果相等,弹出 ALL_LOCAL_FILE 中的对应项目
+                            for file_name in ALL_LOCAL_FILE:
+                                # 如果是文件 , 在本地所有文件中进行查找 , 
+                                # 如果找到且文件大小不相等 , 删掉服务器的版本后重新上传
+                                if os.path.basename (file_name) == name_: # 如果文件相同
+                                    if os.path.getsize (file_name) == int(info_ ['size']):
+                                        # 如果相等,弹出 ALL_LOCAL_FILE 中的对应项目
+                                        ALL_LOCAL_FILE.remove (file_name)
+                                        break
+                                    else:
+                                        # 如果不相等 , 重新上传文件
+                                        FTP.delete (os.path.join (name,name_))
+                                        if upload (file_name,name):
+                                            # 弹出 ALL_LOCAL_FILE 中的对应项目
                                             ALL_LOCAL_FILE.remove (file_name)
-                                            break
-                                        else:
-                                            # 如果不相等 , 重新上传文件
-                                            FTP.delete (os.path.join (name,name_))
-                                            if upload (file_name,name):
-                                                # 弹出 ALL_LOCAL_FILE 中的对应项目
-                                                ALL_LOCAL_FILE.remove (file_name)
 
-                # 第一轮筛选完毕 , 已经将所有大小与本地不符 , 且已经上传过的文件弹出
+            # 第一轮筛选完毕 , 已经将所有大小与本地不符 , 且已经上传过的文件弹出
 
-                # 现在开始上传服务器中没有的项目
+            # 现在开始上传服务器中没有的项目
 
-                for file_name in ALL_LOCAL_FILE:
-                    upload (os.path.basename(file_name),file_name[:10])
-                    # '2019-11-26-Tue-22-42-22'
-                    # [:10] 即 2019-11-26
-                
+            for file_name in ALL_LOCAL_FILE:
+                CODEING_STATE.UPLOADING = True
+                upload (os.path.basename(file_name),file_name[:10])
+                # '2019-11-26-Tue-22-42-22'
+                # [:10] 即 2019-11-26
+            CODEING_STATE.UPLOADING = False
+
             time.sleep (FTP_REFRESH_CLOCK) # 休眠 , 等待下一次循环
 
         except Exception as e:
             print ('ERROR : FTPworker worked in trouble. : {0} ; now retry again.'.format (repr (e)))
 
-
-    # 流程:
-    # 循环:
-    #   判断是否有上传任务
-    #   |     无:
-    #   |       | 重新登录 , 比对本地与服务器的信息
-    #   |       |    有待上传文件:
-    #   |       |        add to list
-    #   |       |     无待上传文件:
-    #   |       |        放弃,等待下一次循环                    
-    #   |     有:
-    #   |       | 放弃 , 等待下一次循环 
-    #  
-
-
-    
     # =======下载操作==========
     # fp = open ('D:\\a.jpg','wb')
     # FTP.retrbinary('RETR ' + '白色.jpg', fp.write, 1024)
     # =================
     # 开始周期循环
     # 文件上传 FTP.storbinary('STOR {path}'.format(), fp, FTP_BUFSIZE)
-
-
 TH_FTPWORKER = TR.Thread(target=FTPWORKER)
 TH_FTPWORKER.start()
 TH_LIST.append (TH_FTPWORKER)
@@ -305,11 +321,87 @@ TH_LIST.append (TH_FTPWORKER)
 # https://blog.csdn.net/qq_28506941/article/details/88345511
 # ========================== FTP 监听开始 ==========================
 
+def videodecoder ():
+    while True:
+        try:
+            # 开始遍历文件夹
+            ALL_FILE = [] # 等待编码文件的绝对路径
+            for i_dir in os.listdir ('video'):
+                if os.path.isdir (os.path.join('video',i_dir)):
+                    # 如果是目录 则说明是日期目录
+                    # 继续往下
+                    for i_file in os.listdir (os.path.join('video',i_dir)):
+                        if i_file [-4:] == '.h264':
+                            # 如果是h.264文件,说明还没有被转码
+                            # 添加到目录中
+                            ALL_FILE.append (os.path.realpath(os.path.join(
+                                'video',i_dir,i_file
+                            )))
 
-while not EXIT: # 循环事件
-    time.sleep (1) # 循环事件
-else:
-    for i in TH_LIST:
-        i.join () # 等待线程结束
-        time.sleep (1)
-    quit ()
+            # 将所有待转码文件进行转码
+            for i in ALL_FILE:
+                try: # 防止出现编码失败
+                    if os.system ('./ffmpeg -i {input} -codec copy {output}'.format (input=i,output=i[:-4] + '.mp4')) == 0:
+                        # =0 说明成功
+                        os.remove (i) # 删除 '.h264' 文件
+                    else:
+                        # 如果失败
+                        try:
+                            os.remove (i[:-4] + '.mp4')
+                        except:
+                            pass
+                except:
+                    pass # 跳过
+        except:
+            print ('ERROE : Videocoder thread work failed. Restarting .')
+
+        time.sleep (FTP_REFRESH_CLOCK * 2)
+
+TH_VIDEOCODER = TR.Thread (target=videodecoder)
+TH_VIDEOCODER.start ()
+TH_LIST.append (TH_VIDEOCODER)
+# FFmepg和x264的编译用于硬件加速: https://blog.csdn.net/weixin_37272286/article/details/93893959
+# ffmpeg -i 2018.h264 -codec copy 2018.mp4 直接复制视频流
+# ============== 转码监听开始 ================
+
+def autoremove (): # 自动删除程序
+    global REMOVE_CYCLE
+    REMOVE_CYCLE *= 86400
+    while True:
+        try:
+            # 遍历文件
+            for i_dir in os.listdir ('video'): # 遍历 video 目录下的日期目录
+                # 当前位于日期目录
+                if os.path.isdir ('video/' + i_dir):
+                    # 如果是日期文件夹 , 则进入
+                    for i_file in os.listdir ('video/' + i):
+                        # i [:-4] 去掉后缀
+                        createTime = time.mktime(time.strptime (i,'%Y-%m-%d-%a-%H-%M-%S')) 
+                        # ↑ 此方法将时间转为秒
+                        # Such as: 2019-11-28-Thu-22-06-58 => 1574950018.0
+
+                        if (time.time () - createTime) > REMOVE_CYCLE: # 如果大于删除的周期
+                            os.remove (os.path.join ('video',i_dir,i_file)) # 删除文件
+
+
+
+            
+        except:
+            print ('ERROR : Remove thread worked failed. Restart now.')
+
+TH_AUTOREMOVE = TR.Thread (target=autoremove)
+TH_AUTOREMOVE.start ()
+TH_LIST.append (TH_AUTOREMOVE)
+
+# =========== 自动清理 =============
+
+
+
+
+while True:
+    try:
+        time.sleep (0.1)
+    except SystemExit:
+        sys.exit ()
+
+    
